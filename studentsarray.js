6c6d07586fd195b55572c718fd17f0437eeb4e31
basmaabdou <117539268+basmaabdou@users.noreply.github.com>
2023-04-14 01:04:47 +0200

    array

---
+++ studentsarray.js
@@ -0,0 +1,31 @@
+export const students = [{
+    id: 1,
+    name: "Basma "
+}, {
+    id: 2,
+    name: "Ahmed "
+}, {
+    id: 3,
+    name: "salma "
+}, {
+    id: 4,
+    name: "Ali "
+}, {
+    id: 5,
+    name: "Yasser "
+}, {
+    id: 6,
+    name: "Asmaa "
+}, {
+    id: 7,
+    name: "Noran "
+}, {
+    id: 8,
+    name: "Samah "
+}, {
+    id: 9,
+    name: "Zinab "
+}, {
+    id: 10,
+    name: "Mohamed "
+}]
